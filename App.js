4633a61b98607ee819d2401aec78d3922db2933e
extraci <thelinuxpointsup@gmail.com>
2023-02-03 17:42:09 +0530

    updates

---
+++ App.js
@@ -0,0 +1,74 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Dimensions } from 'react-native';
+// import type {Node} from 'react';
+// import { Router, Scene } from 'react-native-router-flux'
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet, 
+    Button,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+import Default from './components/default.js';
+import Login from './components/login.js';
+import Register from './components/register.js';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
+
+let width = Dimensions.get('window').width;
+let height = Dimensions.get('window').height;
+
+const App =  () => {
+    
+    return (
+        <>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="Home"
+                        component={Default}
+                        options={{title: 'Welcome'}}
+                    />
+                    <Stack.Screen
+                        name="Login"
+                        component={Login}
+                        options={{title: 'Log In'}}
+                    />
+                    <Stack.Screen
+                        name="Register"
+                        component={Register}
+                        options={{title: 'Sign Up'}}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+
+    );
+
+};
+
+export default App;
---
+++ components/default.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+import type {Node} from 'react';
+import { Component } from 'react'
+
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet, 
+    Button,
+    Text,
+    useColorScheme,
+    View,
+    InputAccessoryView, 
+    TouchableOpacity,
+    TextInput
+} from 'react-native';
+
+import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+let width = Dimensions.get('window').width; 
+let height = Dimensions.get('window').height; 
+
+const styles = StyleSheet.create({
+    baseText: {
+        fontFamily: "Cochin"
+    },
+    titleText0: {
+        fontSize: 50,
+        padding:50,
+        color:"black",
+        fontWeight: "bold"
+    },
+    titleText1: {
+        fontSize: 20,
+        color:"black",
+        textAlign:"center",
+        display:"flex",
+        width:width
+    },
+    button:{
+        alignItems: 'center',
+        backgroundColor: '#0377ff',
+        padding: 20,
+        marginBottom: 10,
+        color:"black",
+        borderRadius:10
+    }
+});
+
+// The Initial Component
+const Default = ({navigation}) => {
+
+	return (
+        <View style={{
+                flex:1,
+                backgroundColor:"#fff",
+                alignItems: "center",
+                display: "flex",
+                width: width,
+                height:height
+            }}>
+            <Text style={styles.titleText0}>DDS</Text>
+
+            <Text style={styles.titleText1}>Fly to your doorstep , the future of delivery is here.</Text>
+            
+            <View style={{
+                position:"absolute",
+                bottom: 80
+            }}>
+
+                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
+                        
+                        <Text style={{color:"white"}}>Login</Text>
+                    </TouchableOpacity>
+                    
+                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Register')} >
+                        <Text style={{color:"white"}}>Register</Text>
+                    </TouchableOpacity>
+                
+                <Text style={{paddingTop:50,color:"black"}}>Build By Prakash choudhary</Text>
+            </View>
+        </View>
+	);
+
+}
+
+export default Default;
